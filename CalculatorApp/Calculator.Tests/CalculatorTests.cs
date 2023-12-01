3b4e723bde05ec1f1886623c6e7bb73c7f26d22f
unknown <chirapathleo.saelim@gmail.com>
2023-12-01 21:27:19 +0700

    first commit

---
+++ CalculatorApp/Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [TestCase(5, 3, 8)]
+        [TestCase(-1, -1, -2)]
+        public void Add_WhenCalled_ReturnsSum(double a, double b, double expected)
+        {
+            double result = calculator.Add(a, b);
+            Assert.AreEqual(expected, result);
+        }
+
+        public void Subtract_WhenCalled_ReturnsSum(double a, double b, double expected)
+        {
+            double result = calculator.Subtract(a, b);
+            Assert.AreEqual(expected, result);
+        }
+
+        public void Multiply_WhenCalled_ReturnsSum(double a, double b, double expected)
+        {
+            double result = calculator.Multiply(a, b);
+            Assert.AreEqual(expected, result);
+        }
+
+        public void Divide_WhenCalled_ReturnsSum(double a, double b, double expected)
+        {
+            double result = calculator.Divide(a, b);
+            Assert.AreEqual(expected, result);
+        }
+        // Additional tests for Subtract, Multiply, Divide...
+    }
+}
---
+++ CalculatorApp/CalculatorApp.UITests/CalculatorUITests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CalculatorUITests
+{
+    [TestFixture]
+    public class CalculatorUITests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5000"); // Replace with your calculator's URL
+        }
+
+        [Test]
+        public void TestAddition()
+        {
+            driver.FindElement(By.Id("input1")).SendKeys("5");
+            driver.FindElement(By.Id("input2")).SendKeys("3");
+            driver.FindElement(By.Id("addButton")).Click();
+
+            var result = driver.FindElement(By.Id("result")).Text;
+            Assert.AreEqual("8", result);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
